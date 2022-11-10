1da7b8bf439e484e54c951142ac3da17dbd0581a
Josh <joshuawen97@gmail.com>
2022-11-10 17:32:31 +0700

    server error fixed - in function getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import { addUtxoInputs } from '@tatumio/tatum';
 
-export default function Home() {
+export default function Home({ data }) {
 	return (
 		<div className="">
 			<Head>
@@ -14,6 +15,35 @@ export default function Home() {
 			<Header />
 			{/* Banner */}
 			<Banner />
+			{/* Main section */}
+			<main className="max-w-7xl mx-auto px-8 sm:px-16">
+				<section className="pt-6">
+					<h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+					{/* pull data from server */}
+					{data?.map((item) => (
+						<h1>{item.location}</h1>
+					))}
+				</section>
+			</main>
+			{/*  */}
 		</div>
 	);
+}
+
+const fetch = require('node-fetch');
+const https = require('https');
+const httpsAgent = new https.Agent({
+	rejectUnauthorized: false,
+});
+
+export async function getStaticProps() {
+	const res = await fetch('https://links.papareact.com/pyp', {
+		agent: httpsAgent,
+	});
+	const data = await res.json();
+	return {
+		props: {
+			data: data,
+		},
+	};
 }
