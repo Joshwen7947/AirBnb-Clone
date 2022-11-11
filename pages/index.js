48b535a5a81d59d3f29c7a6d29e7b1e17f501868
Josh <joshuawen97@gmail.com>
2022-11-11 16:51:38 +0700

    converted from typescript to js for easier use of json

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,36 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
-import { addUtxoInputs } from '@tatumio/tatum';
+import SmallCard from '../components/SmallCard';
 
-export default function Home({ data }) {
+function Home({ data }) {
 	return (
 		<div className="">
 			<Head>
 				<title>Airbnb</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
-			{/* Header */}
 			<Header />
-			{/* Banner */}
 			<Banner />
-			{/* Main section */}
 			<main className="max-w-7xl mx-auto px-8 sm:px-16">
 				<section className="pt-6">
 					<h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-					{/* pull data from server */}
-					{data?.map((item) => (
-						<h1>{item.location}</h1>
+
+					{data?.map(({ img, distance, location }) => (
+						<SmallCard
+							key={img}
+							img={img}
+							distance={distance}
+							location={location}
+						/>
 					))}
 				</section>
 			</main>
-			{/*  */}
 		</div>
 	);
 }
 
+export default Home;
 const fetch = require('node-fetch');
 const https = require('https');
 const httpsAgent = new https.Agent({
@@ -43,7 +44,7 @@ export async function getStaticProps() {
 	const data = await res.json();
 	return {
 		props: {
-			data: data,
+			data,
 		},
 	};
 }
