2ecbaa5a98f95ade5ac4757947ab1aa8e5c3c1bf
Josh <joshuawen97@gmail.com>
2022-11-12 15:36:48 +0700

    little

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,22 @@ function Home({ data }) {
 			<main className="max-w-7xl mx-auto px-8 sm:px-16">
 				<section className="pt-6">
 					<h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+					<div className="grid grid-cols-4 p-15">
+						{data?.map(({ img, distance, location }) => (
+							<SmallCard
+								key={img}
+								img={img}
+								distance={distance}
+								location={location}
+							/>
+						))}
+					</div>
+				</section>
+			</main>
 
-					{data?.map(({ img, distance, location }) => (
-						<SmallCard
-							key={img}
-							img={img}
-							distance={distance}
-							location={location}
-						/>
-					))}
+			<main className="max-w-7xl mx-auto px-8 sm:px-16">
+				<section className="pt-10">
+					<h2 className="text-4xl font-semibold pb-5">Best Destinations</h2>
 				</section>
 			</main>
 		</div>
