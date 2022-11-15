5d738fafd6d9a1266aa2b4e84fffcc74961ed150
Josh <joshuawen97@gmail.com>
2022-11-15 10:27:48 +0700

    medium cards done

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 import {
 	SearchIcon,
 	GlobeAltIcon,
@@ -8,16 +8,18 @@ import {
 } from '@heroicons/react/solid';
 export default function Header() {
 	return (
-		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 			{/* left */}
 			<div className="relative flex items-center h-10 cursor-pointer my-auto">
 				<Image
-					src="https://links.papareact.com/qd3"
-					layout="fill"
-					alt=""
-					objectFit="contain"
-					objectPosition="left"
-				/>
+                    src="https://links.papareact.com/qd3"
+                    alt=""
+                    fill
+                    sizes="100vw"
+                    style={{
+                        objectFit: "contain",
+                        objectPosition: "left"
+                    }} />
 			</div>
 			{/* middle */}
 			<div className="flex items-center border-2 rounded-full py-2 md:shadow-sm">
@@ -38,5 +40,5 @@ export default function Header() {
 				</div>
 			</div>
 		</header>
-	);
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import SmallCard from '../components/SmallCard';
+import MediumCards from '../components/MediumCards';
 
-function Home({ data }) {
+function Home({ data1, data2 }) {
 	return (
 		<div className="">
 			<Head>
@@ -15,8 +16,8 @@ function Home({ data }) {
 			<main className="max-w-7xl mx-auto px-8 sm:px-16">
 				<section className="pt-6">
 					<h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-					<div className="grid grid-cols-4 p-15">
-						{data?.map(({ img, distance, location }) => (
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  p-15">
+						{data1?.map(({ img, distance, location }) => (
 							<SmallCard
 								key={img}
 								img={img}
@@ -26,11 +27,13 @@ function Home({ data }) {
 						))}
 					</div>
 				</section>
-			</main>
-
-			<main className="max-w-7xl mx-auto px-8 sm:px-16">
 				<section className="pt-10">
-					<h2 className="text-4xl font-semibold pb-5">Best Destinations</h2>
+					<h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+					<div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+						{data2?.map(({ img, title }) => (
+							<MediumCards key={img} img={img} title={title} />
+						))}
+					</div>
 				</section>
 			</main>
 		</div>
@@ -38,20 +41,33 @@ function Home({ data }) {
 }
 
 export default Home;
-const fetch = require('node-fetch');
-const https = require('https');
-const httpsAgent = new https.Agent({
+
+const fetch1 = require('node-fetch');
+const https1 = require('https');
+const httpsAgent1 = new https1.Agent({
+	rejectUnauthorized: false,
+});
+
+const fetch2 = require('node-fetch');
+const https2 = require('https');
+const httpsAgent2 = new https2.Agent({
 	rejectUnauthorized: false,
 });
 
 export async function getStaticProps() {
-	const res = await fetch('https://links.papareact.com/pyp', {
-		agent: httpsAgent,
+	const res1 = await fetch1('https://links.papareact.com/pyp', {
+		agent: httpsAgent1,
 	});
-	const data = await res.json();
+	const res2 = await fetch2('https://links.papareact.com/zp1', {
+		agent: httpsAgent2,
+	});
+
+	const data1 = await res1.json();
+	const data2 = await res2.json();
 	return {
 		props: {
-			data,
+			data1,
+			data2,
 		},
 	};
 }
